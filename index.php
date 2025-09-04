a7b29fc968a094772991ea047f012433f615a7f8
LANDRYMASTER <angebile146@gmail.com>
2025-09-04 19:48:37 +0000

    front-end

---
+++ index.php
@@ -0,0 +1,138 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background: #0a1725ff; 
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+        }
+        body img{
+            position: fixed;
+            top: 30px;
+            right: 30px;
+            border: 2px solid white;
+            box-shadow: 0 0 10px white;
+            width: 80px;
+            aspect-ratio: 1;
+            border-radius: 40px;
+            cursor: pointer;
+        }
+        .formulaire {
+            padding: 10px;
+            display: flex;
+        }
+        form{
+            border: 2px solid white;
+            border-radius: 15px;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            box-shadow: 0 0 10px white;
+            background: white;
+            transform: scale(1.2);
+            min-width: 300px;
+        }
+        h1{
+            color: #0a1725ff;
+            text-align: center;
+            transition: 0.3s ease-in;
+            animation: sintille 1s infinite linear;
+        }
+        label{
+             margin: 5px 0;
+
+        }
+        form input[type="text"], input[type="password"]{
+            border: none;
+            padding: 10px;
+            box-shadow: inset 0 0 3px rgba(0, 0, 0, 0.479);
+            border-radius: 15px;
+            font-size: 18;
+            outline: none;
+
+        }
+        form input[type="submit"]{
+            margin: 10px 0;
+            border: none;
+            background-color: rgb(141, 72, 2);
+            color: white;
+            font-size: 15px;
+            padding: 8px;
+            letter-spacing: 2px ;
+            cursor: pointer;
+            transition: 0.5s ease-in;
+        }
+        form input[type="submit"]:hover {
+            background-color: #0a1725ff;
+        }
+        form a{
+            text-align: end;
+            font-size: 12px;
+        }
+        .icon{
+            color: rgb(141, 72, 2);
+            margin-top: 10px;
+            display: inline;
+            text-align: center;
+        }
+        .icon span{
+            cursor: pointer;
+        }
+        .icon span svg{
+            fill: rgb(141, 72, 2);
+            transition: 0.5s ease-in;
+        }
+        .icon span svg:hover{
+            fill: #0a1725ff;
+        }
+
+        @keyframes sintille {
+            0%, 50%{
+                color: #0a1725ff;
+                text-shadow: 0 0 5px rgb(205, 106, 8);
+            }
+            100%{
+                color: rgb(205, 106, 8);
+                text-shadow: 0 0 5px #0a1725ff;
+            }
+        }
+    </style>
+</head>
+<body>
+    
+    <div>
+        <img src="label .png" alt="">
+    </div>
+    <div class="formulaire">
+        <div>
+            <form action="">
+                <h1>Connexion</h1>
+                <label for="">Name</label>
+                <input type="text">
+                <label for="">Password</label>
+                <input type="password">
+                <input type="submit" value="connecter">
+                <a href="">Password forwad</a>
+                <div class="icon">
+                    <span><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 30 30"><path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path></svg></span>
+                    <span><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 24 24"><path d="M12.545,12.151L12.545,12.151c0,1.054,0.855,1.909,1.909,1.909h3.536c-0.607,1.972-2.101,3.467-4.26,3.866 c-3.431,0.635-6.862-1.865-7.19-5.339c-0.34-3.595,2.479-6.62,6.005-6.62c1.002,0,1.946,0.246,2.777,0.679 c0.757,0.395,1.683,0.236,2.286-0.368l0,0c0.954-0.954,0.701-2.563-0.498-3.179c-1.678-0.862-3.631-1.264-5.692-1.038 c-4.583,0.502-8.31,4.226-8.812,8.809C1.945,16.9,6.649,22,12.545,22c6.368,0,8.972-4.515,9.499-8.398 c0.242-1.78-1.182-3.352-2.978-3.354l-4.61-0.006C13.401,10.24,12.545,11.095,12.545,12.151z"></path></svg></span>
+                    <span><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 30 30"><path d="M15,3C8.373,3,3,8.373,3,15c0,6.016,4.432,10.984,10.206,11.852V18.18h-2.969v-3.154h2.969v-2.099c0-3.475,1.693-5,4.581-5 c1.383,0,2.115,0.103,2.461,0.149v2.753h-1.97c-1.226,0-1.654,1.163-1.654,2.473v1.724h3.593L19.73,18.18h-3.106v8.697 C22.481,26.083,27,21.075,27,15C27,8.373,21.627,3,15,3z"></path></svg></span>
+                </div>
+            </form>
+        </div>
+    </div>
+    
+<?php 
+
+?>
+
+
+</body>
+</html>
